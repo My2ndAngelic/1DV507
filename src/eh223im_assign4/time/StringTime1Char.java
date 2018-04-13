b3f0620e77d12a166a85cb482dc835c9a2a47593
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2018-04-13 13:04:11 +0200

    Update
    Update

--- src/eh223im_assign4/time/StringTime1Char.java
+++ src/eh223im_assign4/time/StringTime1Char.java
@@ -3,46 +3,47 @@ package eh223im_assign4.time;
 import java.util.Random;
 
 public class StringTime1Char {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        int k = 0;
-        long timeBefore = 0;
-        long timeAfter = 0 ;
+        int k;
+        long timeBefore ;
+        long timeAfter;
 
-        // Appending 1 character string
-        do {
-            String s = "";
-            String test = String.valueOf((char) (Character.toUpperCase(new Random().nextInt(90-65+1)+65)));
-            timeBefore = System.currentTimeMillis();
-            for (int i = 0; i < k; i++){
-                s += test;
-            }
-            timeAfter = System.currentTimeMillis();
-            k++;
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } while (timeAfter - timeBefore < 1);
+        for (int a = 0; a < 100; a++) {
+            k = 0;
+            // Appending 1 character string
+            do {
+                String s = "";
+                String test = String.valueOf((char) (Character.toUpperCase(new Random().nextInt(90 - 65 + 1) + 65)));
+                timeBefore = System.currentTimeMillis();
+                for (int i = 0; i < k; i++) {
+                    s += test;
+                }
+                timeAfter = System.currentTimeMillis();
+                k++;
+                System.out.print("\r"+"String concatenation: "+k);
+//                Thread.sleep(500);
+//                System.out.print("\r"+k);
+            } while (timeAfter - timeBefore < 1000);
 
+            System.out.print("\n");
 
-        System.out.print(k+ " ");
+            System.gc();
+        }
 
-        System.gc();
-        k = 0;
+        System.out.println();
 
-        do {
-            StringBuilder s = new StringBuilder();
-            String test = String.valueOf((char) (Character.toUpperCase(new Random().nextInt(90-65+1)+65)));
-            timeBefore = System.currentTimeMillis();
-            for (int i = 0; i < k; i++){
-                s.append(test);
-            }
-            timeAfter = System.currentTimeMillis();
-            k++;
-        } while (timeAfter - timeBefore < 1);
-
-        System.out.println(k);
+//        do {
+//            StringBuilder s = new StringBuilder();
+//            String test = String.valueOf((char) (Character.toUpperCase(new Random().nextInt(90-65+1)+65)));
+//            timeBefore = System.currentTimeMillis();
+//            for (int i = 0; i < k; i++){
+//                s.append(test);
+//            }
+//            timeAfter = System.currentTimeMillis();
+//            k++;
+//        } while (timeAfter - timeBefore < 1);
+//
+//        System.out.println(k);
     }
 }
