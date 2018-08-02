4f728276306da20d671a32e67249953d3f2f79f1
Evan Huynh <8387105+My2ndAngelic@users.noreply.github.com>
2018-08-03 00:58:32 +0200

    TEST

--- src/eh223im_assign4/time/SortingTest.java
+++ src/eh223im_assign4/time/SortingTest.java
@@ -54,9 +54,41 @@ public class SortingTest {
         }
     }
 
+    public static void QuickTest(int numbers, int max){
+        double time;
+        int[] initArr = new int[numbers];
+        for (int i = 0; i < numbers; i++) {
+            initArr[i] = new Random().nextInt(max);
+        }
+        int[] sortingArr = Arrays.copyOf(initArr,initArr.length);
+
+        System.gc();
+        long t1 = System.nanoTime();
+        Arrays.sort(sortingArr);
+        long t2 = System.nanoTime();
+        time = t2 - t1;
+
+        t1 = System.nanoTime();
+        int[] checkArr = Arrays.copyOf(sortingArr,sortingArr.length);
+        Arrays.sort(checkArr);
+
+        if (Arrays.equals(sortingArr,checkArr)) {
+            System.out.println("Quick Sort;" + numbers + ";" + max + ";" + time / 1000000000.0);
+        } else {
+            System.out.println("Quick Sort: Error");
+        }
+    }
+
+
     public static void main(String[] args) {
-        int num = 100000000;
-        int max = 100000000;
+        int num = 1000000000;
+        int max = 1000000000;
+
+        for (int i = 0; i < num; i+=1000000) {
+            QuickTest(i, max);
+        }
+
+        System.out.println("\n---\n");
 
         for (int i = 0; i < num; i+=1000000) {
             MergeTest(i, max);
